d36d338c9b74c4028b28047144f289b85caf8a50
Daniela Diaz <danieladiaz@Danielas-MacBook.local>
2018-04-18 17:27:38 -0500

    All letters with movement work in the scroll view section

--- Proyecto_Final/Proyecto_Final/ViewControllerScroll.swift
+++ Proyecto_Final/Proyecto_Final/ViewControllerScroll.swift
@@ -36,7 +36,7 @@ class ViewControllerScroll: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var imgY: UIImageView!
     
    
-    //Reproduce el video
+    //Reproduce el video J
     @IBAction func btJ(_sender : Any){
         //Voy a sacar el path del video que puse
         if let path = Bundle.main.path(forResource: "j", ofType: "mp4"){
@@ -50,12 +50,62 @@ class ViewControllerScroll: UIViewController, UIScrollViewDelegate {
         }
     }
     
+    //Reproduce el video K
+    @IBAction func btK(_ sender: Any) {
+        if let path2 = Bundle.main.path(forResource: "k", ofType: "mp4"){
+            let videoK = AVPlayer(url: URL(fileURLWithPath : path2))
+            let videoPlayerK = AVPlayerViewController()
+            videoPlayerK.player = videoK
+            
+            present(videoPlayerK, animated: true, completion: {
+                videoK.play()
+            })
+        }
+    }
+    
+    //Reproduce el video Q
+    @IBAction func btQ(_ sender: Any) {
+        if let path3 = Bundle.main.path(forResource: "q", ofType: "mp4"){
+            let videoQ = AVPlayer(url: URL(fileURLWithPath : path3))
+            let videoPlayerQ = AVPlayerViewController()
+            videoPlayerQ.player = videoQ
+            
+            present(videoPlayerQ, animated: true, completion: {
+                videoQ.play()
+            })
+        }
+    }
+    
+    //Reproduce el video X
+    @IBAction func btX(_ sender: Any) {
+        if let path4 = Bundle.main.path(forResource: "x", ofType: "mp4"){
+            let videoX = AVPlayer(url: URL(fileURLWithPath : path4))
+            let videoPlayerX = AVPlayerViewController()
+            videoPlayerX.player = videoX
+            
+            present(videoPlayerX, animated: true, completion: {
+                videoX.play()
+            })
+        }
+    }
+    
+    //Reproduce el video Z
+    @IBAction func btZ(_ sender: Any) {
+        if let path5 = Bundle.main.path(forResource: "z", ofType: "mp4"){
+            let videoZ = AVPlayer(url: URL(fileURLWithPath : path5))
+            let videoPlayerZ = AVPlayerViewController()
+            videoPlayerZ.player = videoZ
+            
+            present(videoPlayerZ, animated: true, completion: {
+                videoZ.play()
+            })
+        }
+    }
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-      
         
     }
 
