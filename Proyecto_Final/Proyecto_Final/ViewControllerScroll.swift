1ba150f531d1c2dd8b5b2da43b4b0adb3568bdad
Daniela Diaz <danieladiaz@Danielas-MacBook.local>
2018-04-22 17:42:11 -0500

    Created a new scroll view and it is now functional

--- Proyecto_Final/Proyecto_Final/ViewControllerScroll.swift
+++ Proyecto_Final/Proyecto_Final/ViewControllerScroll.swift
@@ -10,31 +10,6 @@ import UIKit
 import AVKit
 
 class ViewControllerScroll: UIViewController, UIScrollViewDelegate {
-
-    //Intento hacer que cada imagen tenga zoom
-    @IBOutlet weak var scrollView: UIView!
-    @IBOutlet weak var imgA: UIImageView!
-    @IBOutlet weak var imgB: UIImageView!
-    @IBOutlet weak var imgC: UIImageView!
-    @IBOutlet weak var imgD: UIImageView!
-    @IBOutlet weak var imgE: UIImageView!
-    @IBOutlet weak var imgF: UIImageView!
-    @IBOutlet weak var imgG: UIImageView!
-    @IBOutlet weak var imgH: UIImageView!
-    @IBOutlet weak var imgI: UIImageView!
-    @IBOutlet weak var imgL: UIImageView!
-    @IBOutlet weak var imgM: UIImageView!
-    @IBOutlet weak var imgN: UIImageView!
-    @IBOutlet weak var imgO: UIImageView!
-    @IBOutlet weak var imgP: UIImageView!
-    @IBOutlet weak var imgR: UIImageView!
-    @IBOutlet weak var imgS: UIImageView!
-    @IBOutlet weak var imgT: UIImageView!
-    @IBOutlet weak var imgU: UIImageView!
-    @IBOutlet weak var imgV: UIImageView!
-    @IBOutlet weak var imgW: UIImageView!
-    @IBOutlet weak var imgY: UIImageView!
-    
    
     //Reproduce el video J
     @IBAction func btJ(_sender : Any){
@@ -113,9 +88,7 @@ class ViewControllerScroll: UIViewController, UIScrollViewDelegate {
     
     }
     
-    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return self.imgA
-    }
+
 
     /*
     // MARK: - Navigation
